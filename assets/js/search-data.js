7c43082ecea8b04a20e0590b4409fd796dda68fb
jaisidhsingh <75247817+jaisidhsingh@users.noreply.github.com>
2025-11-22 14:49:24 +0000

    Deploying to gh-pages from @ jaisidhsingh/jaisidhsingh.github.io@b1cf09dcc8b0fd9726a6691c7f91f34c2a4e43cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "books-the-godfather",
+  },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
